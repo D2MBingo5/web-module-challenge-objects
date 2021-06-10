2764d919e0980512d314fdf3129eb8761f1118bf
Leu Family <romanianfobjr@gmail.com>
2021-06-10 15:07:28 -0500

    solved task 3 and 4

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ const reviews = [
 Using the reviews array above:
   1. log only Julius' feedback to the console - no function needed 
 */
-
+console.log(reviews[5].feedback);
 
 
 
@@ -86,7 +86,8 @@ Using the reviews array above do the following: (no function needed)
   1. Following the same format (name, rating, feedback), add a new fictitious review object to the reviews array
   2. log the whole array to the console, make sure the new review is inside of it   
 */
-
+reviews.push("Damian", 3.5, "Pretty cool, I guess, pretty cool.");
+console.log(reviews);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
